74b6192ae2af727a97f939cb4edefd0c0180de37
Mike North <michael.l.north@gmail.com>
2025-12-21 22:53:29 -0800

    chore: skip interactive tests in CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,15 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    testTimeout: 10000,
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+      'test/interactive-export-flow.test.ts',
+      'test/integration/cli-smoke.test.ts'
+    ],
   },
 })
